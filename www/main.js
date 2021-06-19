3644f874427a6e0d1d9d6da5f9bd1e3392a94dd8
DanilaCoffee <kofeikun@gmail.com>
2021-06-19 21:19:11 +0300

    slightly modified version
    Added the ability to switch menu items in the portfolio section and write the logic of the feedback form

--- www/main.js
+++ www/main.js
@@ -2,7 +2,7 @@ const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     let id = anchor.getAttribute('href')
     document.querySelector(id + '').scrollIntoView({
       behavior: 'smooth',
@@ -129,5 +129,68 @@ rightButton.addEventListener('click', (e) => {
         slicklist.innerHTML = '<img src="images/p0.png" alt=""><img src="images/p0.png" alt=""><img src="images/p0.png" alt=""><img src="images/p1.png" alt="">'
       }, 200)
     break
+  }
+})
+
+let devMainTab = document.getElementsByClassName('devInfo')[0]
+let devProjectsTab = document.getElementsByClassName('devInfo')[1]
+let devContactsTab = document.getElementsByClassName('devInfo')[2]
+
+devProjectsTab.style.display = 'none'
+devContactsTab.style.display = 'none'
+
+let ourteamItem1 = document.getElementsByClassName("ourteamItem")[0]
+let ourteamItem2 = document.getElementsByClassName("ourteamItem")[1]
+let ourteamItem3 = document.getElementsByClassName("ourteamItem")[2]
+
+ourteamItem1.addEventListener('click', () => {
+  devMainTab.style.display = 'block'
+  devProjectsTab.style.display = 'none'
+  devContactsTab.style.display = 'none'
+  ourteamItem1.className = 'ourteamItem active'
+  ourteamItem2.className = 'ourteamItem'
+  ourteamItem3.className = 'ourteamItem'
+})
+
+ourteamItem2.addEventListener('click', () => {
+  devMainTab.style.display = 'none'
+  devProjectsTab.style.display = 'block'
+  devContactsTab.style.display = 'none'
+  ourteamItem1.className = 'ourteamItem'
+  ourteamItem2.className = 'ourteamItem active'
+  ourteamItem3.className = 'ourteamItem'
+})
+
+ourteamItem3.addEventListener('click', () => {
+  devMainTab.style.display = 'none'
+  devProjectsTab.style.display = 'none'
+  devContactsTab.style.display = 'block'
+  ourteamItem1.className = 'ourteamItem'
+  ourteamItem2.className = 'ourteamItem'
+  ourteamItem3.className = 'ourteamItem active'
+})
+
+let sendError = [true, true, true]
+
+for (let i = 0; i < 3; i++) {
+  document.getElementsByClassName('inp')[i].addEventListener('change', (e) => {
+    if (e.currentTarget.value != '') {
+      document.getElementsByClassName('red')[i].innerHTML = ''
+      sendError[i] = false
+    } else {
+      document.getElementsByClassName('red')[i].innerHTML = '*'
+      sendError[i] = true
+    }
+  })
+}
+
+document.getElementsByClassName('contactusButton')[0].addEventListener('click', () => {
+  if (sendError[0] || sendError[1] || sendError[2]) {
+    alert('Введите все необходимые поля\nдля отправки сообщения')
+  } else {
+    let name = document.getElementsByClassName('inp')[0].value
+    let email = document.getElementsByClassName('inp')[1].value
+    let message = document.getElementsByClassName('inp')[2].value
+    alert('Спасибо за сообщение!\n\n' + name + '\n' + message + '\n\n' + email)
   }
 })
